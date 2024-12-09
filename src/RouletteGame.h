c97ed680e395df114c2235c2144730e8e423c175
Micah Meadows <micahm213@gmail.com>
2024-12-08 22:37:34 -0500

    make roulette game state

---
+++ src/RouletteGame.h
@@ -0,0 +1,17 @@
+#ifndef ROULETTEGAME_H
+#define ROULETTEGAME_H
+
+enum class RouletteState {
+    RS_UNSTARTED = 0,
+    RS_PLAYING = 1,
+    RS_GAME_OVER = 2,
+    RS_MAX = 3
+};
+
+struct RouletteGame {
+    int bulletPosition;
+    int shotsFired;
+    RouletteState state = RouletteState::RS_UNSTARTED;
+};
+
+#endif
