d67976bbad84713636c8830d2380e0fb84bcb84d
Micah Meadows <micahm213@gmail.com>
2024-12-08 22:37:21 -0500

    Move pins to h file

---
+++ src/Pins.h
@@ -0,0 +1,16 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define SERVO_PIN 23
+#define BTN_PIN 22
+#define TEST_LED_PIN 13
+#define RED_LED_PIN 2
+#define BLUE_LED_PIN 4
+
+#define RIGHT_BTN_PIN 21
+#define MIDDLE_BTN_PIN 32
+// #define LEFT_BTN_PIN 32 // something else than 32
+
+#define IR_RECV_PIN 15
+
+#endif
